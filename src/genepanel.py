fe859548bbaf7819b507b7661d5f85fbf1365c8b
hassanfa <hassan.foroughi@gmail.com>
2019-05-15 15:52:18 +0200

    feat: added exon range expansion

--- src/genepanel.py
+++ src/genepanel.py
@@ -24,6 +24,47 @@ LOG = logging.getLogger(__name__)
 
 __version__ = 0.01
 
+def parseIntSet(nputstr=""):
+    '''
+    Expand range of numbers.
+    https://stackoverflow.com/questions/712460/interpreting-number-ranges-in-python
+    '''
+
+    selection = set()
+    invalid = set()
+    # tokens are comma seperated values
+    tokens = [x.strip() for x in nputstr.split(',')]
+    for i in tokens:
+        if len(i) > 0:
+            if i[:1] == "<":
+                i = "1-%s"%(i[1:])
+        try:
+            # typically tokens are plain old integers
+            selection.add(int(i))
+        except:
+            # if not, then it might be a range
+            try:
+                token = [int(k.strip()) for k in i.split('-')]
+                if len(token) > 1:
+                    token.sort()
+                    # we have items seperated by a dash
+                    # try to build a valid range
+                    first = token[0]
+                    last = token[len(token)-1]
+                    for x in range(first, last+1):
+                        selection.add(x)
+            except:
+                # not an int and not a range...
+                invalid.add(i)
+    # Report invalid tokens before returning valid selection
+    if len(invalid) > 0:
+        LOG.error("Invalid set: " + str(invalid))
+        return None
+    else:
+        return selection
+# end parseIntSet
+
+
 @click.command()
 @click.option(
     '--log-level',
@@ -74,20 +115,44 @@ def genepanel(log_level, reference, input_json, output_bed, strand_match):
         LOG.error("Invalid input json string")
         raise click.Abort()
 
+    if not input_json['genename']:
+        LOG.error('Input string must have genename entry')
+        raise click.Abort()
+
+    # if there is exons entry, expand the range
+    if input_json['exons']:
+        exon_range = parseIntSet(input_json['exons'])
+        if exon_range is None:
+            LOG.error('Incorrect exon range. Example: <3,4-7,10-12,24')
+            raise click.Abort()
+        input_json['exons'] = ",".join(str(x) for x in exon_range)
+        LOG.info('Exon ranges converted to %s', exon_range)
+
+    # if there is no output file specified
     if not output_bed:
-        output_bed = input_json['genename']+".bed"
+        output_bed = '_'.join(str(x) for x in input_json.values())
+        for ch in ",:-":
+            output_bed = output_bed.replace(ch,'_')
+        output_bed += ".bed"
         LOG.info("Setting output file name as %s",output_bed) 
     else:
         LOG.debug("Output file is %s", output_bed)
 
+    # read input reference file
     try:
         LOG.debug("Loading reference file into a dataframe")
         reference_df = pandas.read_csv(
             reference, delimiter='\t', encoding='utf-8')
+        # create a new df by removing all characters after "." in transcript field
+        # i.e. "name" column
+        transcript_df = reference_df["name"].str.split(".", n = 1, expand = True)
+        # replace transcript names with first value
+        reference_df['name'] = transcript_df[0]
     except:
         LOG.error("Couldn't load reference file into dataframe")
         raise click.Abort()
 
+    # filter reference file based on input json
     try:
         LOG.debug("Extracting regions based on genename")
         df = copy.deepcopy(
@@ -95,14 +160,20 @@ def genepanel(log_level, reference, input_json, output_bed, strand_match):
                 'chrom', 'exonStarts', 'exonEnds', 'strand', 'name2', 'name',
                 'exonCount'
             ]])
+        if 'transcript' in input_json.keys():
+            if input_json['transcript']:
+                df = df.loc[df['name'] == input_json['transcript']]
     except:
         LOG.error("Extracting genename from reference file failed")
         raise click.Abort()
 
+    # if entries found in the reference, then exit
     if df.shape[0] == 0:
         LOG.warning(
             "No enteries for input string was found in reference file.")
+        raise click.Abort()
 
+    # prepare filtered dataframe as bed file to write to output
     try:
         LOG.debug("Converting dataframe to pybedtools object and expanding exonStarts and exonEnds")
         df_bed = pybedtools.BedTool.from_dataframe(df).expand(c="2,3").sort()
@@ -110,6 +181,7 @@ def genepanel(log_level, reference, input_json, output_bed, strand_match):
         LOG.error("Bed objet creation failed")
         raise click.Abort()
 
+    # collapse columns 4(strand), 5 (gene i.e. name2), and 6 (transcript i.e. name)
     try:
         LOG.debug("Merging bed regions and collapsing disctint strand, genename, transcript")
         df_bed.merge(c='4,5,6', o="distinct").saveas(output_bed)
